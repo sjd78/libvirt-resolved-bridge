8da6bdc21667c47071e883c55288e16aaf3b969e
Scott J Dickerson <sdickers@redhat.com>
2026-01-22 14:33:06 -0500

    the bridge is working!
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
 	"log"
 	"net"
 	"os"
@@ -25,8 +24,21 @@ type NetworkXML struct {
 	} `xml:"ip"`
 }
 
+// DBus struct types for systemd-resolved
+// SetLinkDNS expects: a(iay) - array of (int32 family, []byte address)
+type dnsEntry struct {
+	Family  int32
+	Address []byte
+}
+
+// SetLinkDomains expects: a(sb) - array of (string domain, bool routing_only)
+type domainEntry struct {
+	Domain      string
+	RoutingOnly bool
+}
+
 const (
-	libvirtDest    = "org.libvirt.virt"
+	libvirtDest    = "org.libvirt"
 	resolvedDest   = "org.freedesktop.resolve1"
 	resolvedObject = "/org/freedesktop/resolve1"
 )
@@ -38,8 +50,12 @@ func main() {
 	}
 	defer conn.Close()
 
-	rule := "type='signal',sender='org.libvirt.virt',interface='org.libvirt.Network'"
-	conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, rule)
+	// Listen for NetworkEvent signals from org.libvirt.Connect interface
+	rule := "type='signal',sender='org.libvirt',path='/org/libvirt/QEMU',interface='org.libvirt.Connect',member='NetworkEvent'"
+	call := conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, rule)
+	if call.Err != nil {
+		log.Fatalf("Failed to add match rule: %v", call.Err)
+	}
 
 	signals := make(chan *dbus.Signal, 10)
 	conn.Signal(signals)
@@ -61,21 +77,33 @@ func main() {
 }
 
 func handleSignal(conn *dbus.Conn, sig *dbus.Signal) {
-	if len(sig.Body) < 2 {
-		return
-	}
-	netName, ok1 := sig.Body[0].(string)
-	event, ok2 := sig.Body[1].(int32)
-	if !ok1 || !ok2 {
+	// NetworkEvent signal: (o network, i event)
+	if sig.Name != "org.libvirt.Connect.NetworkEvent" {
 		return
 	}
 
+	if len(sig.Body) < 2 {
+		return
+	}
+
+	netPath, ok1 := sig.Body[0].(dbus.ObjectPath)
+	event, ok2 := sig.Body[1].(int32)
+	if !ok1 || !ok2 {
+		log.Printf("Unexpected signal body types: %T, %T", sig.Body[0], sig.Body[1])
+		return
+	}
+
+	// https://libvirt.org/html/libvirt-libvirt-network.html#virNetworkEventLifecycleType
 	switch event {
-	case 0: // Started
-		log.Printf("Network '%s' detected as STARTED", netName)
-		processNetwork(conn, netName)
-	case 1: // Stopped
-		log.Printf("Network '%s' detected as STOPPED", netName)
+	case 0:
+		log.Printf("Network '%s' detected as DEFINED", netPath)
+	case 1:
+		log.Printf("Network '%s' detected as UNDEFINED", netPath)
+	case 2: // Started (VIR_NETWORK_EVENT_STARTED)
+		log.Printf("Network '%s' detected as STARTED", netPath)
+		processNetwork(conn, netPath)
+	case 3: // Stopped (VIR_NETWORK_EVENT_STOPPED)
+		log.Printf("Network '%s' detected as STOPPED", netPath)
 	}
 }
 
@@ -87,20 +115,19 @@ func parseNetworkXML(data []byte) (*NetworkXML, error) {
 	return &config, nil
 }
 
-func processNetwork(conn *dbus.Conn, netName string) {
-	objPath := dbus.ObjectPath(fmt.Sprintf("/org/libvirt/virt/network/%s", netName))
-	netObj := conn.Object(libvirtDest, objPath)
+func processNetwork(conn *dbus.Conn, netPath dbus.ObjectPath) {
+	netObj := conn.Object(libvirtDest, netPath)
 
 	var xmlData string
 	err := netObj.Call("org.libvirt.Network.GetXMLDesc", 0, uint32(0)).Store(&xmlData)
 	if err != nil {
-		log.Printf("Could not get XML for network %s: %v", netName, err)
+		log.Printf("Could not get XML for network %s: %v", netPath, err)
 		return
 	}
 
 	config, err := parseNetworkXML([]byte(xmlData))
 	if err != nil {
-		log.Printf("Failed to parse XML for %s: %v", netName, err)
+		log.Printf("Failed to parse XML for %s: %v", netPath, err)
 		return
 	}
 
@@ -108,8 +135,10 @@ func processNetwork(conn *dbus.Conn, netName string) {
 	domain := config.Domain.Name
 	dnsIP := config.IP.Address
 
+	log.Printf("Network config - bridge: %s, domain: %s, dns: %s", bridge, domain, dnsIP)
+
 	if bridge == "" || (domain == "" && dnsIP == "") {
-		log.Printf("No bridge or domain/DNS IP found for %s", netName)
+		log.Printf("No bridge or domain/DNS IP found for %s", netPath)
 		return
 	}
 
@@ -119,6 +148,7 @@ func processNetwork(conn *dbus.Conn, netName string) {
 func applyToResolved(conn *dbus.Conn, ifName, domain, dnsIP string) {
 	iface, err := net.InterfaceByName(ifName)
 	if err != nil {
+		log.Printf("Could not find interface %s: %v", ifName, err)
 		return
 	}
 
@@ -128,15 +158,23 @@ func applyToResolved(conn *dbus.Conn, ifName, domain, dnsIP string) {
 	if dnsIP != "" {
 		ip := net.ParseIP(dnsIP).To4()
 		if ip != nil {
-			log.Printf("Setting DNS for interface %s to %s", ifName, ip.String())
-			dnsEntry := []interface{}{int32(2), []byte(ip)}
-			resObj.Call("org.freedesktop.resolve1.Manager.SetLinkDNS", 0, ifIndex, []interface{}{dnsEntry})
+			log.Printf("Setting DNS for interface %s (idx %d) to %s", ifName, ifIndex, ip.String())
+			// AF_INET = 2 for IPv4
+			dnsEntries := []dnsEntry{{Family: 2, Address: []byte(ip)}}
+			call := resObj.Call("org.freedesktop.resolve1.Manager.SetLinkDNS", 0, ifIndex, dnsEntries)
+			if call.Err != nil {
+				log.Printf("Failed to set DNS: %v", call.Err)
+			}
 		}
 	}
 
 	if domain != "" {
-		log.Printf("Setting domains for interface %s to %s", ifName, domain)
-		domainEntry := []interface{}{domain, true}
-		resObj.Call("org.freedesktop.resolve1.Manager.SetLinkDomains", 0, ifIndex, []interface{}{domainEntry})
+		log.Printf("Setting domain for interface %s (idx %d) to %s", ifName, ifIndex, domain)
+		// routing_only=true means this domain is only for routing, not search
+		domainEntries := []domainEntry{{Domain: domain, RoutingOnly: true}}
+		call := resObj.Call("org.freedesktop.resolve1.Manager.SetLinkDomains", 0, ifIndex, domainEntries)
+		if call.Err != nil {
+			log.Printf("Failed to set domains: %v", call.Err)
+		}
 	}
 }
